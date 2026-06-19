8abc65f2a32c596ed884258100757d219ddb85a7
Patrick <94patrick.ng@gmail.com>
2026-06-19 10:43:11 +0800

    Back-end Test 1 Initial commit

--- index.js
+++ index.js
@@ -16,6 +16,39 @@ const pool = new Pool({
   },
 });
 
+app.get("/novels/:id", async (req, res) => {
+  const id = req.params.id;
+  const client = await pool.connect();
+  try {
+    const query = 'SELECT * FROM novels WHERE id = $1';
+    const result = await client.query(query, [id]);
+    res.json(result.rows);
+  } catch (err) {
+    console.error("error: ", err.message);
+    res.status(500).json({ error: err.message });
+  } finally {
+    client.release();
+  }
+});
+
+app.put("/novels/:id", async (req, res) => {
+  const id = req.params.id;
+  const editData = req.body;
+  const client = await pool.connect();
+  try {
+    const updateQuery = 'UPDATE novels SET title = $1, author = $2, year_published = $3 WHERE id = $4 RETURNING id,title,author,year_published';
+    const queryData = [editData.title, editData.author, editData.year_published, id];
+    const result = await client.query(updateQuery, queryData);
+
+    res.json({ "data: ": result.rows })
+  } catch (err) {
+    console.error("error: ", err.message);
+    res.status(500).json({ error: err.message });
+  } finally {
+    client.release();
+  }
+});
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
